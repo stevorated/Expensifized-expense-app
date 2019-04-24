4c38a5d52e7a9eff97dddd69ff561e074f3ab087
stevorated <garbers8@gmail.com>
2019-04-24 21:45:02 +0300

    CHANGED SERVER FOR PRODUCTION

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const publicPath = path.join(__dirname,'..', 'public' )
-
+const port = process.env.PORT || 3000
 app.use(express.static(publicPath))
 
 app.get('*', (req,res)=>{
     res.sendFile(publicPath, 'index.html')
 })
 
-app.listen(3000, ()=> {
-    console.log('server is up on port 3000')
+app.listen(port, ()=> {
+    console.log(`server is up on port ${port}`)
 })
